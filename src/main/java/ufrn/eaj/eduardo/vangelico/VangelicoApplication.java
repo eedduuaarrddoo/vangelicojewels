234ee7aa34388a1f35f8b0c6fdc8ac4260f72c5d
Eduardo <edu_ardu@hotmail.com.br>
2021-07-04 15:09:39 -0300

    Initial commit

---
+++ src/main/java/ufrn/eaj/eduardo/vangelico/VangelicoApplication.java
@@ -0,0 +1,13 @@
+package ufrn.eaj.eduardo.vangelico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VangelicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VangelicoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ufrn/eaj/eduardo/vangelico/VangelicoApplicationTests.java
@@ -0,0 +1,13 @@
+package ufrn.eaj.eduardo.vangelico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VangelicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
